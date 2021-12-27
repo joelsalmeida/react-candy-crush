673eedee075d5019068802c6187379520afc6a71
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-12-27 17:45:28 -0300

    Cria estado "currentCandyArrangement", responsavel por carregar lista de doces aleatoriamente. Cria funcao "createBoard" responsavel por gerar lista de doces

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+import { useEffect, useState } from 'react';
+
+const width = 8;
+const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow'];
+
 function App() {
+  const [currentCandyArrangement, setCurrentCandyArrangement] = useState();
+
+  const createBoard = () => {
+    const randomCandyArrangement = [];
+
+    for (let index = 0; index < width * width; index++) {
+      const randomCandyColorsIndex = Math.floor(Math.random() * candyColors.length);
+      const randomCandy = candyColors[randomCandyColorsIndex];
+
+      randomCandyArrangement.push(randomCandy);
+    }
+
+    setCurrentCandyArrangement(randomCandyArrangement);
+  };
+
+  useEffect(() => createBoard(), []);
+
   return <div className="App">Candy Crush</div>;
 }
 
